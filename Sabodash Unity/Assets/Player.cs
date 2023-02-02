cfd23e95d61314303f67e873c53f08a6fdb9d678
felipejannarone <116711040+squanky1372@users.noreply.github.com>
2023-02-01 22:40:22 -0600

    Cleanup
    Cleaned up movement script. Moved all walls to be children of the first wall.

--- Sabodash Unity/Assets/Player.cs
+++ Sabodash Unity/Assets/Player.cs
@@ -1,67 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Parameters
+    float speed = 0.1f;
+    float flyspeed = 0.15f;
+    float maxvel = 8f;
+    float jumpstrength = 10f;
+
+    //Variables
     public Rigidbody2D rigbod;
+    private bool jumped = false;
 
     void Start()
     {
         rigbod = GetComponent<Rigidbody2D>();
     }
 
-    private bool jumped = false;
     void MoveAnywhere()
     {
-        var speed = 0.1f;
-        var flyspeed = 0.15f;
-        var maxvel = 8f;
-        var jumpstrength = 10f;
-
         var horizontalInc = Convert.ToInt32(Input.GetKey("right")) - Convert.ToInt32(Input.GetKey("left"));
         var verticalInc = Convert.ToInt32(Input.GetKey("up")) - Convert.ToInt32(Input.GetKey("down"));
         var dir = new Vector2(horizontalInc * speed, verticalInc * flyspeed);
-        //UnityEngine.Debug.Log(dir);
+        //Debug.Log(dir);
         rigbod.velocity = rigbod.velocity + dir;
         if(rigbod.velocity.magnitude > maxvel){
             rigbod.velocity = rigbod.velocity / rigbod.velocity.magnitude * maxvel;
         }
-
-        if (Input.GetKey(KeyCode.Space) && !jumped)
-        {
+        if (Input.GetKey(KeyCode.Space) && !jumped){
             rigbod.velocity = rigbod.velocity + new Vector2(0, jumpstrength);
             jumped = true;
         }
-        if (!Input.GetKey(KeyCode.Space))
-        {
+        if (!Input.GetKey(KeyCode.Space)){
             jumped = false;
         }
-        
-    }
-
-    private bool buttonWasDown = false;
-    void parseButtons(){ 
-        bool buttonDown = Input.GetAxis("Fire1s") > 0;
-        if(buttonWasDown){
-            buttonWasDown = buttonDown;
-            return;
-        }
-        buttonWasDown = buttonDown;
-
-        var inputs = new string[]{"Fire1", "Fire2", "Fire3", "Fire4"};
-
-        //if (Input.GetAxis("Fire1") > 0) Debug.Log("fire");
-        for(int i = 1; i < 4; i++){
-            if(Input.GetAxis(inputs[i]) > 0){
-                
-            }
-        }
-
-        
     }
 
     // Update is called once per frame
@@ -72,9 +47,6 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Enemy")
-        {
-
-        }
+        if(collision.collider.tag == ""){}
     }
 }
