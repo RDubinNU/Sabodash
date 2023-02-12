591317d1393df16d378a375e4bd8e86eda3008a0
RDubinNU <reecedubin2024@u.northwestern.edu>
2023-02-12 14:26:57 -0600

    Updated Movement
    - Fixed unable to change direction at max speed
    - Added delays for flying and bugfixed double jumping on ground
    - Reorganized the velocity caps to allow for overcapping on jump but not on fly
    - Various bugfixing
    
    Movement system should be done now with only need for tuning

--- Sabodash Unity/Assets/Player.cs
+++ Sabodash Unity/Assets/Player.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,14 +12,18 @@ public class Player : MonoBehaviour
     float horizontal_accel_speed = 0.05f;
     float maxvel_x = 6f;
 
-    float fly_cap = 6f;
-    float fly_accel = 0.3f;
-    float jumpstrength = 2.5f;
+    float max_vel_y = 6f;
+    float fly_accel = 0.05f;
+    float jumpstrength = 8f;
+    float last_jump = 0f;
+    float fly_delay = 0.25f;
+    float jump_cd = 0.1f;
 
     // Variables
     public Rigidbody2D rigbod;
     public BoxCollider2D boxcollider;
     public SpriteRenderer sprite;
+    private bool grounded;
 
     void Start() {
         rigbod = GetComponent<Rigidbody2D>();
@@ -35,6 +36,7 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
+        grounded = IsGrounded();
         MoveAnywhere();
     }
 
@@ -45,15 +47,13 @@ public class Player : MonoBehaviour
         Vector3 raycast_origin = boxcollider.bounds.center + (Vector3)Vector2.down * boxcollider.bounds.extents.y;
         RaycastHit2D ground_raycast = Physics2D.Raycast(raycast_origin, Vector2.down, tolerance);
 
-        Color rayColor;
-
         bool on_ground = false;
-        if (ground_raycast.collider != null)  on_ground = true;
+        if (ground_raycast.collider != null && rigbod.velocity.y <= 0.05)
+        {
+            on_ground = true;
+        }
 
-        if (on_ground) rayColor = Color.green;
-        else  rayColor = Color.red;
-
-        UnityEngine.Debug.DrawRay(raycast_origin, Vector2.down * tolerance, rayColor);
+        Debug.Log(on_ground);
         return (on_ground);
     }
 
@@ -62,28 +62,34 @@ public class Player : MonoBehaviour
         float accel_x = lr.ReadValue<float>();
         accel_x = accel_x * horizontal_accel_speed;
 
+        if (Math.Abs(rigbod.velocity.x + accel_x) > maxvel_x)
+        {
+            accel_x = 0;
+        }
+
         // Flying and jump control
-        bool grounded = IsGrounded();
         float accel_y = 0;
 
-        if (jump.ReadValue<float>() > 0){
-            if (grounded) accel_y = jumpstrength;
-            else accel_y = fly_accel;
+        if (jump.ReadValue<float>() > 0)
+        {
+            if (grounded & Time.time > last_jump + jump_cd)
+            {
+                accel_y = jumpstrength;
+                last_jump = Time.time;
+                Debug.Log("jumped");
+            }
+            else if ((Time.time > last_jump + fly_delay) && rigbod.velocity.y + fly_accel <= max_vel_y)
+            {
+                accel_y = fly_accel;
+            }
         }
 
         // Acceleration Application
-        // Horizontal velocity squash
-        if (Math.Abs(rigbod.velocity.x) > maxvel_x){
-            accel_x = 0;
-        }
-        // Vertical velocity squash
-        if (Math.Abs(rigbod.velocity.y) > fly_cap){
-            accel_y = 0;
-        }
-
         Vector2 acceleration = new Vector2(accel_x, accel_y);
         rigbod.velocity = rigbod.velocity + acceleration;
 
+
+        // Frictional Slowing
         if(grounded && accel_x == 0){
             rigbod.velocity = new Vector2(rigbod.velocity.x * 0.95f, rigbod.velocity.y);
         }
